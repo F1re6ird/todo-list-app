44e239bc88d7ada2c43ed10c83d56753cce2ecb1
Yekini Abdulwahab <104094403+F1re6ird@users.noreply.github.com>
2025-05-11 21:22:40 +0100

    had a little idea for the preview
    would later check for better altenatives, having multiple while loop could be inefficient

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import NotesPreview from './components/NotesPreview'
 
 const App = () => {
   return (
-    <div>
-      <NotesPreview />
+    <div className='flex flex-col items-center p-4 font-mono bg-[#efefef] min-h-svh gap-4'>
+      <NotesPreview title={'What I think about when I talk about running'} preview={'Running is like you are forcing yourself to be in diffuse thinking mode, do you know what diffuse thinking mode might be my friend. My name is Yekini Abdulwahab and you aee my latest friend reader.'} lastEdited={"March 23, 2025. 11:20 PM"} />
+      <NotesPreview title={'What I think about when I talk about running'} preview={'Running is like you are forcing yourself to be in diffuse thinking mode, do you know what diffuse thinking mode might be my friend. My name is Yekini Abdulwahab and you aee my latest friend reader.'} lastEdited={"March 23, 2025. 11:20 PM"} />
+      <NotesPreview title={'What I think about when I talk about running'} preview={'Running is like you are forcing yourself to be in diffuse thinking mode, do you know what diffuse thinking mode might be my friend. My name is Yekini Abdulwahab and you aee my latest friend reader.'} lastEdited={"March 23, 2025. 11:20 PM"} />
+      <NotesPreview title={'What I think about when I talk about running'} preview={'Running is like you are forcing yourself to be in diffuse thinking mode, do you know what diffuse thinking mode might be my friend. My name is Yekini Abdulwahab and you aee my latest friend reader.'} lastEdited={"March 23, 2025. 11:20 PM"} />
     </div>
   )
 }
--- src/components/NotesPreview.jsx
+++ src/components/NotesPreview.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
-const NotesPreview = () => {
-  return (
-    <div>
-      <h1 className='text-3xl'>This is note preview</h1>
-    </div>
-  )
+
+const NotesPreview = ({ title, preview, lastEdited }) => {
+
+    const previewerForWeb = (
+        inputString,
+        previewerWidth = 0,
+        font = "16px Arial"
+    ) => {
+        const canvas = document.createElement("canvas");
+        const context = canvas.getContext("2d");
+        context.font = font;
+
+        let inputStringShadow = inputString.slice();
+
+        while (
+            Number(context.measureText(inputStringShadow).width.toFixed(0)) >
+            previewerWidth * 2
+        ) {
+            let inputStringShadowArray = inputStringShadow.split(" ");
+            inputStringShadowArray.pop();
+            
+            inputStringShadow = inputStringShadowArray.join(" ");
+            inputStringShadow += "..."
+        }
+        return inputStringShadow;
+    };
+
+    // would change preview to content.
+    const [content, setContent] = useState(previewerForWeb(preview, 135))
+
+
+    const notePreviewRef = useRef(null)
+    useEffect(() => {
+        console.log(notePreviewRef.current.offsetWidth)
+        const handleResize = () => {
+            setContent(previewerForWeb(preview, notePreviewRef.current.offsetWidth))
+        }
+        window.addEventListener('resize', handleResize)
+        return () => { window.removeEventListener('resize', handleResize) }
+
+        /*         return () => { notePreviewRef.removeEventListener('resize', handleResize) } */
+    })
+
+
+
+    return (
+        <div ref={notePreviewRef} className='w-full flex flex-col gap-4 p-2 bg-white rounded-lg'>
+            <h1 className='text-2xl'>
+                {title}
+            </h1>
+            <p className='opacity-60'>
+                {content}
+            </p>
+            <p className='text-[0.8rem]'>
+                {lastEdited}
+            </p>
+
+        </div>
+    )
 }
 
 export default NotesPreview
